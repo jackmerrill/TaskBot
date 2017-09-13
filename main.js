cadc6064e3b53fb714bc6b946890477a7d38d3da
yoshifan509 <yoshifan509@gmail.com>
2017-09-13 18:33:53 -0500

    added website and stuff. DOES NOT do anything to the bot. :)

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+const express = require('express');
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+    // ejs render automatically looks in the views folder
+    response.render('index');
+});
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+});
+// pings server every 15 minutes to prevent dynos from sleeping
+setInterval(() => {
+ http.get('http://taskbotdiscord.herokuapp.com');
+}, 900000);
+
 const Discord = require("discord.js");
 const ytdl = require('ytdl-core');
 const client = new Discord.Client();

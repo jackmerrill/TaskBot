c25991f025fec5a09c97543abc76f4ed1fbd6612
yoshifan509 <yoshifan509@gmail.com>
2017-09-13 21:06:15 -0500

    wow thanks

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === ';ping') {
-    const m = await message.channel.send("Ping?");
+    const m = await msg.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   if (msg.content === ';invite') {

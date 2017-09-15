14158909811f73894b1efcee79b19a1eec32af7f
pixelherodev <npreil@pleasanta.tk>
2017-09-14 21:40:28 -0400

    fix music to use ytdl-core

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = express();
 
 var v=null;
 
+var ytdl=require("ytdl-core");
+
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 5000;

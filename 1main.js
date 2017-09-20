ea256d4058a2ed44e3adc0bd8d68daf0f5c67556
Dim <skyuphigh01@gmail.com>
2017-09-20 19:18:10 -0400

    :b:

--- 1main.js
+++ 1main.js
@@ -36,7 +36,7 @@ client.on("guildDelete", guild => {
 });
 
 
-client.on("message"), async message => {
+client.on("message", async message => {
   // This event will run on every single message received, from any channel or DM.
 
   // It's good practice to ignore other bots. This also makes your bot ignore itself
@@ -101,7 +101,7 @@ client.on("message"), async message => {
   }
 
   if(command === 'eval') {
-    if (!config.owners.includes(msg.author.id)) return message.reply('only my owner can use this command.');
+    if (!config.owners.includes(message.author.id)) return message.reply('only my owner can use this command.');
     const code = clean(args.join(' '));
     const start = process.hrtime();
     try {
